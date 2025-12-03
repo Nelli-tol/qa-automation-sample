116fe22b127dabcfc21d0a908f79c9dcc8a6e06a
Nelli-tol <tolibnell@gmail.com>
2025-12-03 06:40:51 -0500

    Add negative login test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -10,3 +10,14 @@ test('Valid user can log in', async ({ page }) => {
 
   await expect(page.locator('h1')).toHaveText('Logged In Successfully');
 });
+
+test('Shows error message for invalid credentials', async ({ page }) => {
+  await page.goto('https://practicetestautomation.com/practice-test-login/');
+
+  await page.locator('#username').fill('wronguser');
+  await page.locator('#password').fill('wrongpassword');
+
+  await page.locator('#submit').click();
+
+  await expect(page.locator('#error')).toContainText('Your username is invalid!');
+});
